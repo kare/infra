851fcc681fab087455a59c2409396e3402551d33
Kare Nuorteva <kare.nuorteva@me.com>
2022-09-02 01:27:04 +0300

    docs: improve tls comments

--- tls.go
+++ tls.go
@@ -2,14 +2,14 @@ package infra
 
 import "crypto/tls"
 
-// TLSConfig returns TLS configuration for kkn.fi server.
+// TLSConfig returns secure TLS configuration for Internet server.
 func TLSConfig(getCert func() func(*tls.ClientHelloInfo) (*tls.Certificate, error)) *tls.Config {
 	conf := &tls.Config{
 		GetCertificate: getCert(),
-		// Causes servers to use Go's default ciphersuite preferences,
-		// which are tuned to avoid attacks. Does nothing on clients.
+		// Causes servers to use Go's default ciphersuite preferences, which
+		// are tuned to avoid attacks. Does nothing on clients.
 		PreferServerCipherSuites: true,
-		// Only use curves which have assembly implementations
+		// Only use curves which have assembly implementations.
 		CurvePreferences: []tls.CurveID{
 			tls.CurveP256,
 			tls.X25519,
@@ -22,10 +22,6 @@ func TLSConfig(getCert func() func(*tls.ClientHelloInfo) (*tls.Certificate, erro
 			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			// Best disabled, as they don't provide Forward Secrecy,
-			// but might be necessary for some clients
-			//tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			//tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
 		},
 	}
 	return conf
