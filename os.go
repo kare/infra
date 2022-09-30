216820e7d790091617f99f29fc1ebf7b70a54fbb
Kare Nuorteva <kare.nuorteva@me.com>
2022-09-30 23:06:07 +0300

    docs: add godoc for infra.GetenvDefault

--- os.go
+++ os.go
@@ -2,6 +2,9 @@ package infra
 
 import "os"
 
+// GetenvDefault returns the value of environment variable called by key. If
+// the value is undefined or in other words empty, then value of given
+// defaultValue is returned.
 func GetenvDefault(key, defaultValue string) string {
 	v := os.Getenv(key)
 	if v == "" {
