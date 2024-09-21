bf84dbb47a7e51158b6475d69611625befa9b695
Kare Nuorteva <kare.nuorteva@me.com>
2024-09-21 17:25:24 +0300

    refactor: rename functions and deprecate old ones

--- tls.go
+++ tls.go
@@ -22,7 +22,32 @@ func GetDevelopmentCert(certFile, keyFile string) GetCertificate {
 }
 
 // GetCertificateFuncFromFiles loads GetCertificate func from given TLS cert files.
+//
+// Deprecated: GetCertificateFuncFromFiles is deprecated. Use GetCertificateFromFiles instead.
 func GetCertificateFuncFromFiles(certFile, keyFile string) GetCertificate {
+	return GetCertificateFromFiles(certFile, keyFile)
+}
+
+// GetCertificateFuncFromLetsEncrypt loads GetCertificate func from given Let's Encrypt Manager.
+//
+// Deprecated: GetCertificateFuncFromFiles is deprecated. Use GetCertificateFromLetsEncrypt instead.
+func GetCertificateFuncFromLetsEncrypt(m *autocert.Manager) GetCertificate {
+	return m.GetCertificate
+}
+
+// GetCertificateFunc loads certificates for development or production depending on return value
+// of function [kkn.fi/infra#IsProduction].
+//
+// Deprecated: GetCertificateFunc is deprecated.
+func GetCertificateFunc(m *autocert.Manager, devCertFile, devKeyFile string) GetCertificate {
+	if IsProduction() {
+		return GetCertificateFromLetsEncrypt(m)
+	}
+	return GetCertificateFuncFromFiles(devCertFile, devKeyFile)
+}
+
+// GetCertificateFromFiles loads GetCertificate func from given TLS cert files.
+func GetCertificateFromFiles(certFile, keyFile string) GetCertificate {
 	return func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
 		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 		if err != nil {
@@ -32,18 +57,9 @@ func GetCertificateFuncFromFiles(certFile, keyFile string) GetCertificate {
 	}
 }
 
-// GetCertificateFuncFromLetsEncrypt loads GetCertificate func from given Let's Encrypt Manager.
-func GetCertificateFuncFromLetsEncrypt(m *autocert.Manager) GetCertificate {
+// GetCertificateFromLetsEncrypt loads GetCertificate func from given Let's Encrypt Manager.
+func GetCertificateFromLetsEncrypt(m *autocert.Manager) GetCertificate {
 	return m.GetCertificate
-}
-
-// GetCertificateFunc loads certificates for development or production depending on return value
-// of function [kkn.fi/infra#IsProduction].
-func GetCertificateFunc(m *autocert.Manager, devCertFile, devKeyFile string) GetCertificate {
-	if IsProduction() {
-		return GetCertificateFuncFromLetsEncrypt(m)
-	}
-	return GetCertificateFuncFromFiles(devCertFile, devKeyFile)
 }
 
 // GoodTLSConfig configures a copy of given [tls.Config] with good settings.
